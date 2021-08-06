79ce32c38d04c1b729bb55505cae11c309060f2c
Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2021-08-06 16:14:00 +0500

    working Mobile sub menu

--- footer.php
+++ footer.php
@@ -1,20 +1,10 @@
 
-
+<?php wp_footer();?>
 <script>
-    function subMenuSlide() {
-			if( jQuery(window).width() < 768 ) {
-				jQuery('.sub-menu').children().click(function(){
-					event.preventDefault();
-					jQuery(this).children('.sub-menu').slideToggle('slow');     
-				});
-			}
-		};
-		window.addEventListener('load', function() {
-			subMenuSlide();
-		});
-		window.addEventListener('resize', function() {
-			subMenuSlide();
-		});
+  jQuery('.navbar-nav').children().click(function(){
+        event.preventDefault();
+        jQuery(this).children('.sub-menu').slideToggle('slow');     
+    });
 </script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
 
 
-<?php wp_footer(); 
+<?php get_footer(); 
